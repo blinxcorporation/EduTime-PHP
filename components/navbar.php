74d5eea8a268e6724e65fc66be732f6ff6db7ab0
benson <bensonmakau2000@gmail.com>
2022-11-04 00:01:27 +0300

    modified navbar

--- components/navbar.php
+++ components/navbar.php
@@ -2,7 +2,7 @@
 include './server.php';
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Timetabling</a>
+  <a class="navbar-brand" href="#">TMS</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -26,19 +26,22 @@ include './server.php';
       </li>
       
    
-      <li class="nav-item dropdown">
+      <li class="nav-item dropdown active">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Profile
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#"><?php echo $fname ." ".$lname?></a>
-          <a class="dropdown-item" href="#">Chair</a>
+          <a class="dropdown-item" href="#">Chairperson</a>
           <div class="dropdown-divider"></div>
-          <a type="button" class="text-danger text-center" href="logout.php">Logout</a>
+          <a class="dropdown-item text-primary" href="#">Edit Profile</a>
         </div>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#">Labaratories</a>
+        <a class="nav-link" href="javascript:void(0)"></a>
+      </li>
+      <li class="nav-item active">
+      <a type="button" class=" btn btn-danger text-light text-center" href="logout.php">Logout</a>
       </li>
       
     </ul>
