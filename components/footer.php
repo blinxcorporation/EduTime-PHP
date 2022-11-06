2669e624dac9f0bfc896e2a968009e1bb0788d72
benson <bensonmakau2000@gmail.com>
2022-11-06 11:47:37 +0300

    updated footer

--- components/footer.php
+++ components/footer.php
@@ -2,8 +2,8 @@
 <footer class="page-footer font-small">
 
   <!-- Copyright -->
-  <div class="footer-copyright text-center py-3 fixed-bottom">© 2022 Copyright:
-    <a href="/"> Maseno University</a>
+  <div class="footer-copyright text-center py-3 fixed-bottom">© <span id="year">2022</span> Copyright:
+    <a href="https://maseno.ac.ke"> Maseno University</a>
   </div>
   <!-- Copyright -->
 
