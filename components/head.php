fc7879d2908a58d921e4d44d3dc68baf41f1f15c
benson <bensonmakau2000@gmail.com>
2022-12-14 21:45:41 +0300

    updated head

--- components/head.php
+++ components/head.php
@@ -3,7 +3,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
 <link rel="apple-touch-icon" sizes="180x180" href="./static/images/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="./static/images/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="./static/images/favicon-16x16.png">
