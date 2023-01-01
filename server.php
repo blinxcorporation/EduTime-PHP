ebe6932ac3b8428c6d50c9468f958b36f6cd9584
benson <bensonmakau2000@gmail.com>
2023-01-02 00:37:23 +0300

    deleted unwanted comment

--- server.php
+++ server.php
@@ -10,7 +10,6 @@ session_start();
 // connect to the database
 try{
 $db = mysqli_connect('localhost', 'benson', 'benson', 'timetable');
-//$db = mysqli_connect('localhost', 'blinxcok_benson', 'aFek]Np@ZVPZ', 'blinxcok_maseno_e_help');
 // echo 'Database Connected Successfully';
 }
 catch(Exception $e) {
