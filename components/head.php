941eba286c83eeb3b8a3337fbf051297c8b46d61
makaubenson <bensonmakau2000@gmail.com>
2023-02-14 21:35:53 +0300

    added favicons

--- components/head.php
+++ components/head.php
@@ -1,6 +1,16 @@
 <!-- Required meta tags -->
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<!--favicons-->
+<link rel="apple-touch-icon" sizes="180x180" href="./static/images/favicons/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./static/images/favicons/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./static/images/favicons/favicon-16x16.png">
+<link rel="manifest" href="./static/images/favicons/site.webmanifest">
+<link rel="mask-icon" href="./static/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="./static/images/favicons/favicon.ico">
+<meta name="msapplication-TileColor" content="#2b5797">
+<meta name="msapplication-config" content="./static/images/favicons/browserconfig.xml">
+<meta name="theme-color" content="#ffffff">
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="./static/css/style.css">
