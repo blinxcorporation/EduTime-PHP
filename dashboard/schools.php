3c52ab903e6d9dc08642401c8ab816cea3e22679
makaubenson <bensonmakau2000@gmail.com>
2023-02-20 18:59:00 +0300

    build add school modal

--- dashboard/schools.php
+++ dashboard/schools.php
@@ -7,6 +7,7 @@ if (!isset($_SESSION['role_id']) || empty($_SESSION['role_id'])) {
   exit;
 }
 
+//deny access to schools.php if user is not an admin
 if (!isset($_SESSION['role_name']) || $_SESSION['role_name'] !== 'Admin') {
   // if the session variable 'role_name' is not set or does not equal 'Admin', deny access and redirect to a non-privileged page
   header("Location: index.php"); // replace 'index.php' with the URL of a non-privileged page
@@ -123,6 +124,7 @@ include '../assets/components/header.php';
                     <li class="breadcrumb-item active" aria-current="page">
                       Schools
                     </li>
+
                   </ol>
                 </nav>
               </div>
@@ -141,16 +143,19 @@ include '../assets/components/header.php';
           <!-- ============================================================== -->
           <div class="row">
             <div class="col-12">
+           
                   <div class="card">
                 <div class="card-body">
    
                   <h5 class="card-title">List of Schools</h5>
-
+                  <input type='button' value='Add a School' name='open-school-modal-btn' class='btn btn-primary float-end open-school-modal-btn m-2'>
                   <div class="table-responsive">
+
                     <table
                       id="zero_config"
                       class="table table-striped table-bordered"
                     >
+                   
                       <thead>
                         <tr>
                           <th>School ID</th>
@@ -265,6 +270,8 @@ include '../assets/components/header.php';
   </div>
 </div>
 
+
+<!--edit school details-->
 <div class="modal fade" id="editSchoolModal" tabindex="-1" role="dialog" aria-labelledby="editSchoolModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
@@ -298,6 +305,36 @@ include '../assets/components/header.php';
   </div>
 </div>
 
+<!-- add new School-->
+<div class="modal fade" id="addSchoolModal" tabindex="-1" role="dialog" aria-labelledby="addSchoolModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="addSchoolModalLabel">Add a School</h5>
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="">
+        <div class="form-group">
+            <label for="recipient-name" readonly class="col-form-label">School Name:</label>
+            <input type="text" name="sch_name"  class="form-control" id="sch_name" required placeholder="e.g School of Computing and Informatics">
+          </div>
+          <div class="form-group">
+            <label for="recipient-name" class="col-form-label">Short Form:</label>
+            <input type="text" name="sch_short_form" class="form-control" id="sch_short_form" required placeholder="e.g Computing">
+          </div>
+          <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancel</button>
+        <button type="submit" class="btn btn-info" name="update-school-details-btn">Submit</button>
+      </div>
+        </form>
+      </div>
+     
+    </div>
+  </div>
+</div>
 
 
     <!-- ============================================================== -->
@@ -327,6 +364,19 @@ include '../assets/components/header.php';
     </script>
 
 <script>
+//add School details modal code
+function openSchoolModal() {
+  $("#addSchoolModal").modal("show");
+}
+let openAddSchoolModalBtn = document.querySelector(".open-school-modal-btn");
+
+openAddSchoolModalBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  openSchoolModal();
+});
+
+
+
 //edit School details modal code
 function editSchoolModal() {
     $("#editSchoolModal").modal("show");
