859ea3dea4371cd6da3f183d29f893c869b52d13
makaubenson <bensonmakau2000@gmail.com>
2023-03-06 12:38:28 +0300

    deleted unwanted files

--- dashboard/get_departments.php
+++
@@ -1,6 +0,0 @@
-<!-- // Get the selected school id
-$selectedSchoolId = mysqli_real_escape_string($db, $_GET['uni_schools']);
-
-    // Retrieve the schools from the database
-    $query = "SELECT * FROM department_details INNER JOIN school_department_details ON school_department_details.department_id = department_details.department_details WHERE school_department_details.school_id = '$selectedSchoolId'";
-    $result = mysqli_query($db, $query); -->
