694c14ba50325b54535aef1fc3f02afe7321a285
Benson <bensonmakau2000@gmail.com>
2023-03-25 23:44:34 +0300

    code to get units for each semester that have been selected by teachers

--- dashboard/timetable.php
+++ dashboard/timetable.php
@@ -20,17 +20,35 @@ $lname = $_SESSION['lname'];
 $name = $_SESSION['fname'] . " ".$_SESSION['lname'];
 $mail = $_SESSION['email'];
 
+
+//Get Selected Semester
+$sem = $_POST['semester_id'];
 //generate timetable function
 function generateTimetable($semester) {
+
   // Query the database to get the list of units that are active and selected by a lecturer
+if($sem == 'SEM1'){
   $units_query = "SELECT * FROM unit_details
   INNER JOIN lecturer_unit_details ON lecturer_unit_details.lecturer_id = unit_details.unit_code 
   INNER JOIN user_details ON user_details.pf_number = lecturer_unit_details.lecturer_id
   INNER JOIN unit_semester_details ON unit_semester_details.unit_id =lecturer_unit_details.unit_id 
   INNER JOIN semester_details ON semester_details.semester_id = unit_semester_details.semester_id 
-  WHERE unit_semester_details.semester_id='$semester'";
-  
-  $result = mysqli_query($db, $units_query);
+  WHERE unit_semester_details.semester_id LIKE 'Y1S1'
+   OR unit_semester_details.semester_id LIKE 'Y2S1'
+   OR unit_semester_details.semester_id LIKE 'Y3S1'
+   OR unit_semester_details.semester_id LIKE 'Y4S1'";
+}elseif ($sem == 'SEM2') {
+  $units_query = "SELECT * FROM unit_details
+  INNER JOIN lecturer_unit_details ON lecturer_unit_details.lecturer_id = unit_details.unit_code 
+  INNER JOIN user_details ON user_details.pf_number = lecturer_unit_details.lecturer_id
+  INNER JOIN unit_semester_details ON unit_semester_details.unit_id =lecturer_unit_details.unit_id 
+  INNER JOIN semester_details ON semester_details.semester_id = unit_semester_details.semester_id 
+  WHERE unit_semester_details.semester_id LIKE 'Y1S2'
+   OR unit_semester_details.semester_id LIKE 'Y2S2'
+   OR unit_semester_details.semester_id LIKE 'Y3S2'
+   OR unit_semester_details.semester_id LIKE 'Y4S2'";
+}
+  $units_result = mysqli_query($db, $units_query);
 
   // Create a two-dimensional array to store the timetable
   $timetable = array(
@@ -41,125 +59,12 @@ function generateTimetable($semester) {
     "Friday" => array()
   );
 
-  // Loop through the list of units
-  while ($row = mysqli_fetch_assoc($result)) {
-    $unit_id = $row['unit_code'];
-    $unit_name = $row['unit_name'];
-    $semester_id = $row['semester_id'];
-    $semester_name = $row['semester_name'];
-    $unit_type = $row['unit_type'];
-    $room_type = $row['room_type'];
-    $department = $row['department'];
 
-    // Determine the capacity of the room based on the type of room and the department
-    if ($room_type == 'Standard') {
-      if ($department == 'IT') {
-        $capacity = 50;
-      } else if ($department == 'Business') {
-        $capacity = 60;
-      } else if ($department == 'Engineering') {
-        $capacity = 70;
-      }
-    } else if ($room_type == 'Laboratory') {
-      if ($department == 'IT') {
-        $capacity = 30;
-      } else if ($department == 'Business') {
-        $capacity = 40;
-      } else if ($department == 'Engineering') {
-        $capacity = 50;
-      }
-    }
-
-    // Determine the duration of the unit based on the type of unit
-    if ($type == 'Theory') {
-      $duration = 2;
-    } else if ($type == 'ICT-Practical') {
-      $duration = 3;
-    } else if ($type == 'ELECT-Practical') {
-      $duration = 4;
-    }
-
-    // Determine the number of groups based on the department and the semester
-    if ($department == 'IT') {
-      if ($semester == 'YEAR 1 SEMESTER 1' || $semester == 'YEAR 1 SEMESTER 2') {
-        $num_groups = 2;
-      } else if ($semester == 'YEAR 2 SEMESTER 1' || $semester == 'YEAR 2 SEMESTER 2') {
-        $num_groups = 3;
-      } else if ($semester == 'YEAR 3 SEMESTER 1' || $semester == 'YEAR 3 SEMESTER 2') {
-        $num_groups = 4;
-      } else if ($semester == 'YEAR 4 SEMESTER 1' || $semester == 'YEAR 4 SEMESTER 2') {
-        $num_groups = 5;
-      }
-    } else if ($department == 'Business') {
-      if ($semester == 'YEAR 1 SEMESTER 1' || $semester == 'YEAR 1 SEMESTER 2') {
-        $num_groups = 2;
-      } else if ($semester == 'YEAR 2 SEMESTER 1' || $semester == 'YEAR 2 SEMESTER 2') {
-        $num_groups = 3;
-      } else if ($semester == 'YEAR3 SEMESTER 1' || $semester == 'YEAR 3 SEMESTER 2') {
-        $num_groups = 4;
-        } else if ($semester == 'YEAR 4 SEMESTER 1' || $semester == 'YEAR 4 SEMESTER 2') {
-        $num_groups = 5;
-        }
-        } else if ($department == 'Engineering') {
-        if ($semester == 'YEAR 1 SEMESTER 1' || $semester == 'YEAR 1 SEMESTER 2') {
-        $num_groups = 2;
-        } else if ($semester == 'YEAR 2 SEMESTER 1' || $semester == 'YEAR 2 SEMESTER 2') {
-        $num_groups = 3;
-        } else if ($semester == 'YEAR 3 SEMESTER 1' || $semester == 'YEAR 3 SEMESTER 2') {
-        $num_groups = 4;
-        } else if ($semester == 'YEAR 4 SEMESTER 1' || $semester == 'YEAR 4 SEMESTER 2') {
-        $num_groups = 5;
-        }
-        }
-        // Loop through the days of the week
-foreach ($timetable as $day => $slots) {
-  // Loop through the time slots of the day
-  for ($i = 0; $i <= 8; $i++) {
-    // Check if the slot is available for all the groups
-    $slot_available = true;
-    for ($j = 1; $j <= $num_groups; $j++) {
-      $group = $department . ' ' . 'GROUP ' . $j;
-      $query = "SELECT COUNT(*) FROM timetable WHERE day='$day' AND slot=$i AND group_name='$group'";
-      $result = mysqli_query($conn, $query);
-      $row = mysqli_fetch_assoc($result);
-      if ($row['COUNT(*)'] > 0) {
-        $slot_available = false;
-      }
-    }
-
-    // Check if the slot is available for the lecturer
-    $query = "SELECT COUNT(*) FROM timetable WHERE day='$day' AND slot=$i AND lecturer='$lecturer'";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_assoc($result);
-    if ($row['COUNT(*)'] > 0) {
-      $slot_available = false;
-    }
-
-    // Check if the slot is available for the room
-    $query = "SELECT COUNT(*) FROM timetable WHERE day='$day' AND slot=$i AND room_type='$room_type' AND capacity>=$num_groups";
-    $result = mysqli_query($conn, $query);
-    $row = mysqli_fetch_assoc($result);
-    if ($row['COUNT(*)'] > 0) {
-      $slot_available = false;
-    }
-
-    // If the slot is available, assign the unit to the slot
-    if ($slot_available) {
-      for ($j = 1; $j <= $num_groups; $j++) {
-        $group = $department . ' ' . 'GROUP ' . $j;
-        $query = "INSERT INTO timetable (day, slot, unit_name, lecturer, room_type, group_name, num_groups) VALUES ('$day', $i, '$unit', '$lecturer', '$room_type', '$group', $num_groups)";
-        mysqli_query($conn, $query);
-      }
-      break;
-    }
-  }
-}
 // Disconnect from the database
-mysqli_close($conn);
+mysqli_close($db);
 
 // Return the timetable
 return $timetable;
-}
 }
 
 
