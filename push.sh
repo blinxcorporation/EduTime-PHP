c2e64bad17f0fe5ec12287f076e68ac0ab06a37f
Benson <bensonmakau2000@gmail.com>
2023-04-06 23:07:34 +0300

    changed script to automate pushing to git

--- push.sh
+++ push.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-echo "Enter commit message:"
-read message
+while [[ $# -gt 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -m|--message)
+        message="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        *)    # unknown option
+        echo "Unknown option: $key"
+        exit 1
+        ;;
+    esac
+done
+
+if [ -z "$message" ]; then
+    echo "Commit message not provided"
+    exit 1
+fi
+
 git add . &&
 git commit -m "$message"
