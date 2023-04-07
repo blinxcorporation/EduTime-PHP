cd5cec4c7973414a6904745549cf67c3e36c45ca
Benson <bensonmakau2000@gmail.com>
2023-04-07 23:34:32 +0300

    Unit details Report
    able to generate reports about units in pdf

--- dashboard/reports.php
+++ dashboard/reports.php
@@ -201,6 +201,67 @@ if (isset($_POST['download-course-btn'])) {
 
         // header('location: ./reports.php');
 }
+
+if (isset($_POST['download-unit-btn'])) {
+    // Set the content type as a downloadable PDF file
+    header('Content-Type: application/pdf');
+    // Set the file name
+    header('Content-Disposition: attachment; filename="unit_details.pdf"');
+
+    // Include the necessary files for creating a PDF
+    require('fpdf/fpdf.php');
+
+    // Create a new PDF document
+    $pdf = new FPDF();
+    $pdf->AddPage();
+
+    // Set the font and font size for the document
+    $pdf->SetFont('Arial', 'B', 14);
+
+    // Add the logo to the document
+    $pdf->Image('images/logo.png', $pdf->GetPageWidth()/2 - 25, 10, 50, 0, 'PNG');
+
+    // Write the title of the document
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(0, 50, '', 0, 1, 'C');
+    $pdf->Cell(0, 10, 'Maseno University', 0, 1, 'C');
+    $pdf->Cell(0, 10, 'Unit Details', 0, 1, 'C');
+
+    // Set the font and font size for the table headers
+    $pdf->SetFont('Arial', 'B', 12);
+
+    // Write the headers of the table
+    $pdf->Cell(15, 10, 'S.NO', 1);
+    $pdf->Cell(25, 10, 'Unit ID', 1);
+    $pdf->Cell(95, 10, 'Unit Name', 1);
+    $pdf->Cell(40, 10, 'Unit Type', 1);
+     $pdf->Ln();
+
+
+    // Query to get the school details
+    $sql = "SELECT * FROM unit_details ORDER BY id ASC";
+    $result = mysqli_query($db, $sql);
+
+    // Set the font and font size for the table rows
+    $pdf->SetFont('Arial', '', 10);
+
+    // Loop through the results and write them to the table
+    if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $pdf->Cell(15, 10, $row['id'], 1);
+        $pdf->Cell(25, 10, $row['unit_code'], 1);
+        $pdf->Cell(95, 10, $row['unit_name'], 1);
+        $pdf->Cell(40, 10, $row['unit_type'], 1);
+        $pdf->Ln();
+    }
+    }
+
+    // Close the database connection and output the PDF
+    mysqli_close($db);
+    $pdf->Output('D', 'unit_details.pdf');
+
+        // header('location: ./reports.php');
+}
     
 
 $pfno = $_SESSION['pfno'];
@@ -333,18 +394,21 @@ include '../assets/components/header.php';
                 </div>
 
                 <div class="col-md-3">
-                    <a href="">
-                        <div class="card card-hover">
-                            <div class="box bg-cyan text-center">
-                                <h1 class="font-light text-white">
-                                    <i class="fa fa-file-excel"></i>
-                                </h1>
-                                <h6 class="text-light">Unit Details</h6>
+                    <a href="#" name="unit-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-cyan text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-cloud-download-alt"></i>
+                                    </h1>
+                                    <h6 class="text-light">Unit Details</h6>
+                                    <input type="submit" name="download-unit-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     </a>
                 </div>
-
 
                 <div class="col-md-3">
                     <a href="">
