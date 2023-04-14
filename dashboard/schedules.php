837a6407160649fd5bfeb743d634a61cc2150d24
makaubenson <bensonmakau2000@gmail.com>
2023-04-14 23:44:49 +0300

    build version 1 personal timetable

--- dashboard/schedules.php
+++ dashboard/schedules.php
@@ -1,6 +1,8 @@
 <?php
-include '../server.php';
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
+include '../server.php';
 if (!isset($_SESSION['role_id']) || empty($_SESSION['role_id'])) {
   // if the session variable 'role_id' is not set or is empty, destroy the session and redirect to the login page
   session_destroy();
@@ -8,18 +10,97 @@ if (!isset($_SESSION['role_id']) || empty($_SESSION['role_id'])) {
   exit;
 }
 
-//deny access to courses.php if user is not a chaiperson
-if (!isset($_SESSION['role_name']) || ($_SESSION['role_name'] !== 'Chairperson' && $_SESSION['role_name'] !== 'Dean' && $_SESSION['role_name'] !== 'Lecturer')) {
-  // if the session variable 'role_name' is not set or does not equal 'Chairperson', deny access and redirect to a non-privileged page
-  header("Location: index.php"); // replace 'index.php' with the URL of a non-privileged page
-  exit;
-}
+//deny access to courses.php if user is not an admin
+if ($_SESSION['role_name'] !== 'Dean' && $_SESSION['role_name'] !== 'Lecturer' &&  $_SESSION['role_name'] !== 'Chairperson') {
+    // if the session variable 'role_name' is not set or does not equal 'Admin', deny access and redirect to a non-privileged page
+    header("Location: index.php"); // replace 'index.php' with the URL of a non-privileged page
+    exit;
+  }
+  
 
-$pfno = $_SESSION['pfno'];
-$fname = $_SESSION['fname'];
-$lname = $_SESSION['lname'];
-$name = $_SESSION['fname'] . " ".$_SESSION['lname'];
-$mail = $_SESSION['email'];
+  //sessions  
+
+  $pfno = $_SESSION['pfno'];
+  $salutation= $_SESSION['salutation'];
+  $fname = $_SESSION['fname'];
+  $lname = $_SESSION['lname'];
+  $name = $_SESSION['fname'] . " ".$_SESSION['lname'];
+  $mail = $_SESSION['email'];
+  
+//Download Personal TT
+if (isset($_POST['download-personal-tt-btn'])) {
+
+    $lec = $_POST['lec_pf'];
+    $lec_name = $_POST['lec_name'];
+    // Set the content type as a downloadable PDF file
+    header('Content-Type: application/pdf');
+    
+    // Set the file name
+    $filename = $lname . '-timetable.pdf';
+    header('Content-Disposition: attachment; filename="' . $filename . '"');
+    
+    // Include the necessary files for creating a PDF
+    require('fpdf/fpdf.php');
+
+    // Create a new PDF document
+    $pdf = new FPDF();
+    $pdf->AddPage();
+
+    // Set the font and font size for the document
+    $pdf->SetFont('Arial', 'B', 14);
+
+    // Add the logo to the document
+    $pdf->Image('images/logo.png', $pdf->GetPageWidth()/2 - 25, 10, 50, 0, 'PNG');
+    
+    // Query to get the school details
+    $sql = "SELECT *
+    FROM unit_room_time_day_allocation_details urtd1
+    INNER JOIN lecturer_unit_details lud ON urtd1.unit_id = lud.unit_id
+    INNER JOIN user_details ud ON lud.lecturer_id = ud.pf_number
+    INNER JOIN unit_room_time_day_allocation_details urtd2 ON urtd1.unit_id = urtd2.unit_id
+    WHERE urtd2.lecturer_id = 'PF01'";
+    $result = mysqli_query($db, $sql);
+
+    // Write the title of the document
+    $pdf->SetFont('Arial', 'B', 16);
+    $pdf->Cell(0, 50, '', 0, 1, 'C');
+    $pdf->Cell(0, 10, 'Maseno University', 0, 1, 'C');
+    $pdf->Cell(0, 10,$salutation." ".$lname."'s"." Personal Timetable", 0, 1, 'C');
+
+    // Set the font and font size for the table headers
+    $pdf->SetFont('Arial', 'B', 12);
+
+    // Write the headers of the table
+    $pdf->Cell(40, 10, 'Unit', 1);
+    $pdf->Cell(50, 10, 'Day', 1);
+    $pdf->Cell(50, 10, 'Room', 1);
+    $pdf->Cell(50, 10, 'Time', 1);
+    $pdf->Ln();
+
+
+    // Set the font and font size for the table rows
+    $pdf->SetFont('Arial', '', 10);
+
+    // Loop through the results and write them to the table
+    if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $pdf->Cell(40, 10, $row['unit_id'], 1);
+        $pdf->Cell(50, 10, $row['weekday'], 1);
+        $pdf->Cell(50, 10, $row['room_id'], 1);
+        $pdf->Cell(50, 10, $row['time_slot_id'], 1);
+        $pdf->Ln();
+    }
+    }
+
+    // Close the database connection and output the PDF
+    mysqli_close($db);
+    $pdf->Output('D', $filename);
+
+        // header('location: ./reports.php');
+}
+    
+
+
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +114,6 @@ include '../assets/components/header.php';
 </head>
 
 <body>
-
     <!-- ============================================================== -->
     <!-- Topbar header - style you can find in pages.scss -->
     <!-- ============================================================== -->
@@ -43,7 +123,6 @@ include '../assets/components/header.php';
     <!-- ============================================================== -->
     <!-- End Topbar header -->
     <!-- ============================================================== -->
-
 
     <!-- ============================================================== -->
     <!-- Left Sidebar - style you can find in sidebar.scss  -->
@@ -70,7 +149,7 @@ include '../assets/components/header.php';
                             <ol class="breadcrumb">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
                                 <li class="breadcrumb-item active" aria-current="page">
-                                    Timetables
+                                    Reports
                                 </li>
 
                             </ol>
@@ -90,73 +169,184 @@ include '../assets/components/header.php';
             <!-- Start Page Content -->
             <!-- ============================================================== -->
             <div class="row">
-                <div class="col-12">
+                <?php
+    if ($_SESSION['role_name'] === 'Admin'){
+    // display the HTML code if the session variable 'role_name' is set to 'Admin'
+    ?>
+                <div class="col-md-3">
+                    <a href="#" name="faculty-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-success text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Faculty Details</h6>
+                                    <input type="submit" name="download-school-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
+                            </div>
 
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">List of Courses</h5>
-                            <table id="dtBasicExample" class="table table-striped table-bordered table-sm"
-                                cellspacing="0" width="100%">
-                                <thead>
-                                    <tr>
-                                        <th>07:00AM-09:00AM</th>
-                                        <th>09:00AM-11:00AM</th>
-                                        <th>11:00AM-13:00PM</th>
-                                        <th>13:00PM-15:00PM</th>
-                                        <th>15:00PM-17:00PM</th>
-                                        <th>17:00PM-19:00PM</th>
-
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <?php
-  if($_SESSION['role_name'] == 'Chairperson'|| $_SESSION['role_name'] == 'Dean'|| $_SESSION['role_name'] == 'Lecturer'){
-      $data_fetch_query = "SELECT * FROM `course_details` 
-      INNER JOIN department_course_details ON department_course_details.course_id = course_details.course_id 
-      INNER JOIN department_details ON department_details.department_id = department_course_details.department_id 
-      INNER JOIN lecturer_department_details ON lecturer_department_details.department_id = department_details.department_id 
-      INNER JOIN user_details ON user_details.pf_number = lecturer_department_details.lecturer_id 
-      WHERE lecturer_department_details.lecturer_id = '$pfno' ";
-      $data_result = mysqli_query($db, $data_fetch_query);
-      if ($data_result->num_rows > 0){
-          while($row = $data_result->fetch_assoc()) {
-              $course_id = $row['course_id'];
-              $course_name = $row['course_name'];
-              $shortname = $row['course_shortform'];
-            //   $school_name = $row['school_name'];
-              $department_id = $row['department_id'];
-              $department_name = $row['department_name'];
-              $date_created = $row['date_added'];
-
-      echo "<tr> <td>" .$course_id.  "</td>";
-      echo "<td>" .$course_name."</td>";
-      echo "<td>" .$shortname."</td>";
-      echo "<td>" .$date_created."</td> </tr>";
-       }
-      
-      }else{
-      echo "<td>"."No Requests Found"."</td>";
-      }
-      
-      } else{
-          echo "<td>"."No Data Found"."</td>";
-      }
-
-?>
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th>Course ID</th>
-                                        <th>Course Name</th>
-                                        <th>Short Form</th>
-                                        <th>Date Added</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                    </div>
+                        </form>
+                    </a>
                 </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="department-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-info text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf "></i>
+                                    </h1>
+                                    <h6 class="text-light">Department Details</h6>
+                                    <input type="submit" name="download-department-btn" class="btn btn-success"
+                                        value="Download" />
+                                </div>
+                            </div>
+
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="course-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-primary text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Course Details</h6>
+                                    <input type="submit" name="download-course-btn" class="btn btn-cyan"
+                                        value="Download" />
+                                </div>
+                            </div>
+
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="unit-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-cyan text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Unit Details</h6>
+                                    <input type="submit" name="download-unit-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="unit-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-secondary text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Lecturer Details</h6>
+
+                                    <input type="submit" name="download-lecturer-btn" class="btn btn-success"
+                                        value="Download" />
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="lecturer-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-cyan text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Room Details</h6>
+                                    <input type="submit" name="download-room-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="timetable.csv" download>
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-info text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-excel"></i>
+                                    </h1>
+                                    <h6 class="text-light">Timetable</h6>
+                                    <a href="timetable.csv" class="btn btn-success" download>Download</a>
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+                <?php
+    }
+    ?>
+
+
+                <!-- personal TT -->
+                <?php
+if ($_SESSION['role_name'] === 'Chairperson' || $_SESSION['role_name'] === 'Lecturer' || $_SESSION['role_name'] === 'Dean'){
+// display the HTML code if the session variable 'role_name' is set to 'Admin'
+?>
+                <div class="col-md-3">
+                    <a href="#" name="lecturer-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-cyan text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Personal Timetable</h6>
+                                    <input type='text' readonly hidden value='<?php echo $pfno; ?>' name='lec_pf'>
+                                    <input type='text' readonly hidden value='<?php echo $name; ?>' name='lec_name'>
+                                    <input type="submit" name="download-personal-tt-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+
+                <div class="col-md-3">
+                    <a href="#" name="lecturer-form">
+                        <form method="POST" action="">
+                            <div class="card card-hover">
+                                <div class="box bg-primary text-center">
+                                    <h1 class="font-light text-white">
+                                        <i class="fa fa-file-pdf"></i>
+                                    </h1>
+                                    <h6 class="text-light">Departmental Timetable</h6>
+                                    <input type="submit" name="download-department-tt-btn" class="btn btn-info"
+                                        value="Download" />
+                                </div>
+                            </div>
+                        </form>
+                    </a>
+                </div>
+                <?php
+}
+?>
+
+
             </div>
+            <!--close row-->
+
         </div>
         <!-- ============================================================== -->
         <!-- End Container fluid  -->
@@ -180,6 +370,7 @@ include '../assets/components/header.php';
     <!-- ============================================================== -->
     <!-- End Wrapper -->
     <!-- ============================================================== -->
+
 
     <!-- ============================================================== -->
     <!-- All Jquery -->
